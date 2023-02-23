da2d7557e78f9749989d1e9a5bb7a49516a2b48a
shai212w <125558755+shai212w@users.noreply.github.com>
2023-02-23 10:53:38 +0200

    Update Password _Strength_Checker.py

--- Password _Strength_Checker.py
+++ Password _Strength_Checker.py
@@ -1,31 +1,19 @@
 import re
-import string
 
 def check_password_strength(password):
     # Check the password length
     if len(password) < 8:
         return 0, "Password is too short. It should be at least 8 characters long."
 
-    # Check if the password contains uppercase and lowercase letters
     if not re.search(r'[A-Z]', password) or not re.search(r'[a-z]', password):
         return 0, "Password should contain both uppercase and lowercase letters."
 
-    # Check if the password contains digits
     if not re.search(r'\d', password):
         return 0, "Password should contain at least one digit."
 
-    # Check if the password contains symbols
     if not re.search(r'[!@#$%^&*(),.?":{}|<>]', password):
         return 0, "Password should contain at least one symbol."
 
-    # Check if the password is a common password
-    with open('common_passwords.txt') as file:
-        common_passwords = file.readlines()
-        common_passwords = [x.strip() for x in common_passwords]
-    if password in common_passwords:
-        return 0, "Password is too common. Please choose a different password."
-
-    # Calculate the password strength score
     score = 0
     score += len(password) * 4
     score += (len(password) - len(re.sub(r'[a-z]', '', password))) * 2
@@ -35,26 +23,23 @@ def check_password_strength(password):
     if len(password) >= 12:
         score += 10
 
-    # Return the password strength score and a message
-    if score < 20:
-        message = "Password is very weak. Please choose a stronger password."
-    elif score < 40:
-        message = "Password is weak. Please choose a stronger password."
-    elif score < 60:
-        message = "Password is medium. You could choose a stronger password."
+    if score < 60:
+        message = "The password is  VERY WEAK. You could choose a stronger password."
     elif score < 80:
-        message = "Password is strong. Good job!"
+        message = "The password is WEAK.You could choose a stronger password!"
+    elif score < 100:
+        message = "The password is STROG. You could improve a stronger password!"
     else:
-        message = "Password is very strong. Great job!"
+        message = "The password is VERY STROG. Great job!"
 
     return score, message
 
-
-
-input = password
-print = password 
-score, message = check_password_strength(password)
-print(f"Password: {password} - Score: {score} - Message: {message}")
-
-
-input = x
+while True:
+    password = input("Enter your password: ")
+    score, message = check_password_strength(password)
+    print("Your password strength score is:", score)
+    print(message)
+    if score >= 80:
+        break
+    else:
+        print("Please enter a stronger  password with a score of at least 80 \n")
